75b77bbbb2a8934e80a4639803aafd2f4610dcd2
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2021-10-20 21:40:39 -0400

    Add files via upload

---
+++ ComboLock (1).java
@@ -0,0 +1,89 @@
+ import java.util.Scanner;
+/**
+* Author: Ashley Zingillioglu
+*/
+public class ComboLock {
+    //Defining Variables and Instance Variables
+    int secret1, secret2, secret3;
+    int d = 0; //dial
+    int number1, number2, number3;
+    int count = 0;
+
+    public ComboLock() {
+    }
+//Method with Variables
+    public ComboLock(int number1, int number2, int number3) {
+        this.secret1 = number1;
+        this.secret2 = number2;
+        this.secret3 = number3;
+    }
+//Setting secret numbers for passcode
+    public void setSecret1(int secret1) {
+        this.secret1 = secret1;
+    }
+
+    public void setSecret2(int secret2) {
+        this.secret2 = secret2;
+    }
+
+    public void setSecret3(int secret3) {
+        this.secret3 = secret3;
+    }
+
+    public void reset() {
+        this.d = 0;
+        this.number1 = 0;
+        this.number2 = 0;
+        this.number3 = 0;
+        this.count = 0;
+    }
+//Turning Right
+    public void turnRight(int ticks) {
+        this.d = ticks;
+        if (count == 0) {
+            number1 = this.d;
+        }
+        if ( == 2) {
+            number3 = this.d;
+        }
+        count++;
+        
+        if (count == 3) {
+            count = 0;
+        }
+    }
+    //Turning Left
+
+    public void turnLeft(int ticks) {
+        this.d = ticks;
+        if (count == 1) {
+            number2 = this.d;
+            count++;
+        }
+    }
+
+    public boolean open() {
+        if (number1 == secret1 && number2 == secret2 && number3 == secret3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+    public int getCurrentNumber() {
+        return d;
+    }
+    
+    public String toString() {
+        return d + "";
+    }
+    public String getSolution () {
+        return secret1 + "-" + secret2 + "-" + secret3;
+    }
+    public boolean equals (ComboLock z) {
+        return (secret1 == c.secret1) && (secret2 == c.secret2) && (secret3 == c.secret3);
+    }
+
+}
+
---
+++ ComboLockDemo.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+public class ComboLockDemo {
+
+    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		ComboLock lock = new ComboLock(23, 5, 15);
+		
+		boolean loop = true;
+		
+		while (loop) {
+			System.out.print("Enter number of ticks to turn to the right (0-39), or an invalid number to quit: ");
+			int number1 = in.nextInt(); // User inputs, computer accepts
+			
+			if (number1<0 || number1>39) {
+			    break;// Have to leave the program because the numbers don't fit within the range. 
+			    
+			}
+			
+			System.out.print("Enter number of ticks to turn to the left (0-39), or an invalid number to quit: ");
+			int number2 = in.nextInt();// User inputs, computer accepts
+			
+			if (number2<0 || number2>39) {
+			    break;// Have to leave the program because the numbers don't fit within the range. 
+			    
+			}
+			
+			System.out.print("Enter number of ticks to turn to the right (0-39), or an invalid number to quit: ");
+			int number3 = in.nextInt(); // User inputs, computer accepts
+			
+			if (number3<0 || number3>39) {
+			    break;// Have to leave the program because the numbers don't fit within the range. 
+			    
+			}
+			
+			lock.turnRight(number1);
+			lock.turnLeft(number2);
+			lock.turnRight(number3);
+			if (lock.open()) { //Success!
+				System.out.println("The lock has been opened!");
+			} 
+			else //Failure!
+			{
+				System.out.println("The lock has not been opened!");
+	
+			}
+		}
+		
+		break;
+	}
+
+}
+

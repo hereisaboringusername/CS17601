4d093a8afcfd5f079483d0c6688dc5a3fada2066
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2021-11-04 17:29:22 -0400

    Update ComboLockDemo.java

--- ComboLockDemo.java
+++ ComboLockDemo.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class ComboLockDemo {
 
-    public static void main(String[] args) {
+    
 
         public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);

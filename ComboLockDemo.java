bc87db21a97425e4e3d07cae3642aff64adfc1bb
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2021-11-15 16:06:17 -0500

    Update ComboLockDemo.java

--- ComboLockDemo.java
+++ ComboLockDemo.java
@@ -2,42 +2,34 @@
 import java.util.Scanner;
 public class ComboLockDemo {
 
-    
-
         public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		ComboLock lock = new ComboLock(23, 5, 15);
+		ComboLock lock = new ComboLock(30, 20, 10);
 		
 		boolean loop = true;
 		
 		while (loop) {
 			System.out.print("Enter number of ticks to turn to the right (0-39), or an invalid number to quit: ");
 			int number1 = in.nextInt(); // User inputs, computer accepts
-			
-			if (number1<0 || number1>39) {
-			    break;// Have to leave the program because the numbers don't fit within the range. 
-			    
+			if (number1 > 39 || number1 < 0) {
+				break;
 			}
+			lock.turnRight(number1);
 			
 			System.out.print("Enter number of ticks to turn to the left (0-39), or an invalid number to quit: ");
 			int number2 = in.nextInt();// User inputs, computer accepts
-			
-			if (number2<0 || number2>39) {
-			    break;// Have to leave the program because the numbers don't fit within the range. 
-			    
+			if (number2 > 39 || number2 < 0) {
+				break;
 			}
+			lock.turnLeft(number2);
 			
 			System.out.print("Enter number of ticks to turn to the right (0-39), or an invalid number to quit: ");
 			int number3 = in.nextInt(); // User inputs, computer accepts
-			
-			if (number3<0 || number3>39) {
-			    break;// Have to leave the program because the numbers don't fit within the range. 
-			    
+			if (number3 > 39 || number3 < 0) {
+				break;
 			}
-			
-			lock.turnRight(number1);
-			lock.turnLeft(number2);
 			lock.turnRight(number3);
+			
 			if (lock.open()) { //Success!
 				System.out.println("The lock has been opened!");
 			} 
@@ -48,7 +40,6 @@ public class ComboLockDemo {
 			}
 		}
 		
-		return;
 	}
 
 }

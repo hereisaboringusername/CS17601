0a0c3822414e6a3098284feb7d603650f3865513
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2021-11-15 16:05:40 -0500

    Update ComboLock (1).java

--- ComboLock (1).java
+++ ComboLock (1).java
@@ -5,8 +5,9 @@
 public class ComboLock {
     //Defining Variables and Instance Variables
     int secret1, secret2, secret3;
-    int d = 0; //dial
+    int d; //dial
     int number1, number2, number3;
+   
     int count = 0;
 
     public ComboLock() {
@@ -16,53 +17,56 @@ public class ComboLock {
         this.secret1 = number1;
         this.secret2 = number2;
         this.secret3 = number3;
-    }
-//Setting secret numbers for passcode
-    public void setSecret1(int secret1) {
-        this.secret1 = secret1;
-    }
-
-    public void setSecret2(int secret2) {
-        this.secret2 = secret2;
-    }
-
-    public void setSecret3(int secret3) {
-        this.secret3 = secret3;
+        this.d = 0;
     }
 
     public void reset() {
         this.d = 0;
-        this.number1 = 0;
-        this.number2 = 0;
-        this.number3 = 0;
+        
         this.count = 0;
     }
+    
 //Turning Right
     public void turnRight(int ticks) {
-        this.d = ticks;
-        if (count == 0) {
-            number1 = this.d;
-        }
-        if ( count == 2) {
-            number3 = this.d;
-        }
-        count++;
         
-        if (count == 3) {
-            count = 0;
+        count++;
+        if (this.d + ticks > 39) {
+        	this.d = this.d + ticks -  40;
         }
+        else {
+        	this.d = this.d + ticks;
+        }
+        
+ 
+        if (count == 1) {
+        	number1 = this.d;
+        }
+        else if (count == 3) {
+        	number3 = this.d;
+        }
+       
     }
     //Turning Left
 
     public void turnLeft(int ticks) {
-        this.d = ticks;
-        if (count == 1) {
-            number2 = this.d;
-            count++;
+    	
+       count++;
+        if (this.d - ticks < 0) {
+        	this.d = this.d - ticks + 40;
         }
+        else this.d = this.d- ticks;
+        
+        number2 = this.d;
     }
-
     public boolean open() {
+    	System.out.println("Combo:");
+    	System.out.println(Integer.toString(secret1));
+    	System.out.println(Integer.toString(secret2));
+    	System.out.println(Integer.toString(secret3));
+    	System.out.println("Numbers:");
+    	System.out.println(Integer.toString(number1));
+    	System.out.println(Integer.toString(number2));
+    	System.out.println(Integer.toString(number3));
         if (number1 == secret1 && number2 == secret2 && number3 == secret3) {
             return true;
         } else {
@@ -78,12 +82,6 @@ public class ComboLock {
     public String toString() {
         return d + "";
     }
-    public String getSolution () {
-        return secret1 + "-" + secret2 + "-" + secret3;
-    }
-    public boolean equals (ComboLock z) {
-        return (secret1 == z.secret1) && (secret2 == z.secret2) && (secret3 == z.secret3);
-    }
-
 }
+
 
